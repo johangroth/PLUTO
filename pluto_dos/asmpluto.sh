187118fa14daf626fcd918fd8514f84a2ab17387
Johan Groth <johangroth1@gmail.com>
2017-03-30 09:18:53 +0100

    Removed duplicated options in asmpluto.sh.

--- pluto_dos/asmpluto.sh
+++ pluto_dos/asmpluto.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # address offset in eeprogrammer is $8000
 
-64tass --mw65c02  --no-monitor -o pluto.bin --flat --no-monitor --line-numbers --list=pluto.lst Tali-Main-B001.asm
-# 64tass --m65c02 -o symoniii.bin --flat --line-numbers --list=symoniii.lst symoniii.asm
+64tass --mw65c02  -o pluto.bin --flat --no-monitor --line-numbers --list=pluto.lst Tali-Main-B001.asm
