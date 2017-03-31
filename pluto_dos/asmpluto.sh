31d1c89bc6626a0dc2e0fbe16ac39349f29f01ad
Johan Groth <johangroth1@gmail.com>
2017-03-31 20:23:21 +0100

    Removed Tali Forth as it is going to be rewritten when Liara Forth is in Beta.

--- pluto_dos/asmpluto.sh
+++ pluto_dos/asmpluto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # address offset in eeprogrammer is $8000
 
-64tass --mw65c02  -o pluto.bin --flat --no-monitor --line-numbers --list=pluto.lst Tali-Main-B001.asm
+64tass --mw65c02  -o pluto.bin --flat --no-monitor --line-numbers --list=pluto.lst main.asm
