f96e3d090124bdb65535303807032c5227a0d6f8
Johan Groth <johangroth1@gmail.com>
2017-05-16 07:36:52 +0100

    Test program reads the RTC.

--- pluto_dos/test.asm
+++ pluto_dos/test.asm
@@ -1,3 +1,4 @@
+
         * = $500
         CHIN = $82F5
         COUT = $8317
@@ -34,174 +35,8 @@
         VIA = $7FC0
         DS1511 = $7FA0
 
-        TIMEOFDAY = $90
-; Read date and time and store in compressed format
-                 
-;DS1511 CONFIGURATION CONSTANTS — Description Order
-;
-        wr_wdmsp =$01                  ;10 ms underflows LSB
-        wr_wdsp  =$00                  ;10 ms underflows MSB
-        wr_dowap =%00000000            ;no alarm date/day IRQ
-        wr_hrsap =%00000000            ;no alarm hour IRQ
-        wr_minap =%00000000            ;no alarm min IRQ
-        wr_secap =%00000000            ;no alarm secs IRQ
-        wr_crbpa =%00000000            ;updates & WDT IRQs off
-        wr_crbpb =%10100010            ;updates & WDT IRQs on
-
-;
-;================================================================================
-;
-;DS1511 CONTROL MASKS — Description Order
-;
-        d11noirq =%00000000            ;disable IRQs & updates
-        d11onirq =%10100010            ;enable IRQs & updates
-        d11aimsk =%00100000            ;enable NVRAM autoincrement
-        dlleomsk =%00011111            ;enable oscillator
-        d11ecmsk =%11100000            ;extract control values
-        d11emmsk =d11ecmsk ^ %11111111 ;extract month value
-        d11ismsk =%00000011            ;IRQ sources
-        d11sqmsk =%01000000            ;no square wave output
-        d11temsk =%10000000            ;stop/start register updates 
-
-
-;================================================================================
-;
-;DALLAS DS1511 REAL-TIME CLOCK REGISTER DEFINITIONS
-;
-        nr_rtc   =32                   ;total registers ($14-$1F reserved)
-
-;
-;
-;                        register offsets...
-;
-        wr_sect  =$00                  ;TOD seconds ($00-$59 BCD)
-        wr_mint  =$01                  ;TOD minutes ($00-$59 BCD)
-        wr_hrst  =$02                  ;TOD hour ($00-$23 BCD)
-        wr_dowt  =$03                  ;day of week ($01-$07 BCD)
-        wr_datt  =$04                  ;date ($01-$31 BCD)
-        wr_mon   =$05                  ;month & control...
-;
-;                        xxxxxxxx
-;                        ||||||||
-;                        |||+++++———> month ($01-$12 BCD)
-;                        ||+————————> 1: enable 32 Khz at SQW when on battery
-;                        |+—————————> 0: enable 32 KHz at SQW when on Vcc
-;                        +——————————> 0: enable oscillator
-;
-        wr_yrlo  =$06                  ;year LSB ($00-$99 BCD)
-        wr_yrhi  =$07                  ;year MSB ($00-$39 BCD)
-        wr_seca  =$08                  ;alarm seconds & IRQ control...
-;
-;                        xxxxxxxx
-;                        ||||||||
-;                        |+++++++———> alarm seconds ($00-$59 BCD)
-;                        +——————————> 1: IRQ once per second
-;
-        wr_mina  =$09                  ;alarm minutes & IRQ control...
-;
-;                        xxxxxxxx
-;                        ||||||||
-;                        |+++++++———> alarm minutes ($00-$59 BCD)
-;                        +——————————> 1: IRQ when TOD secs = alarm secs
-;
-        wr_hrsa  =$0a                  ;alarm hour & IRQ control...
-;
-;                        x0xxxxxx
-;                        | ||||||
-;                        | ++++++———> alarm hour ($00-$23 BCD)
-;                        +——————————> 1: IRQ when TOD secs & mins = alarm secs & mins
-;
-        wr_dowa  =$0b                  ;alarm date/day & IRQ control...
-;
-;                        xxxxxxxx
-;                        ||||||||
-;                        ||++++++———> alarm day ($01-$07 BCD) or date ($01-$31 BCD)
-;                        |+—————————> 0: alarm date set
-;                        |            1: alarm day set
-;                        +——————————> 0: IRQ when TOD & day/date = alarm TOD & day/date
-;                                     1: IRQ when TOD = alarm time
-;
-        wr_wdms  =$0c                  ;watchdog millisecs*10 ($00-$99 BCD)
-        wr_wds   =$0d                  ;watchdog seconds ($00-$99 BCD)
-        wr_cra   =$0e                  ;control register A...
-;
-;                        xxxxxxxx
-;                        ||||||||
-;                        |||||||+———> 1: IRQ pending (read only)
-;                        ||||||+————> 1: IRQ = watchdog timer
-;                        |||||+—————> 1: IRQ = kickstart (read only)
-;                        ||||+——————> 1: IRQ = TOD alarm
-;                        |||+———————> 0: PWR pin = active low
-;                        |||          1: PWR pin = high-Z
-;                        ||+————————> 0: PWR pin = high-Z wo/Vcc present
-;                        ||           1: PWR pin = active low wo/Vcc present
-;                        |+—————————> 1: aux external battery low (read only)
-;                        +——————————> 1: external battery low (read only)
-;
-        wr_crb   =$0f                  ;control register B...
-;
-;                        x0xxxxxx
-;                        | ||||||
-;                        | |||||+———> 0: watchdog generates IRQ
-;                        | |||||      1: watchdog generates reset
-;                        | ||||+————> 1: watchdog IRQ/reset enabled
-;                        | |||+—————> 1: kickstart IRQ enabled
-;                        | ||+——————> 1: TOD alarm IRQ enabled
-;                        | |+———————> 1: TOD alarm wakeup enabled
-;                        | +————————> 1: NVRAM address autoincrement enabled
-;                        +——————————> 0: TOD & date register update disabled
-;                                     1: TOD & date register update enabled
-;
-        wr_nvra  =$10                  ;NVRAM address port ($00-$FF)
-        wr_rsva  =$11                  ;reserved
-        wr_rsvb  =$12                  ;reserved
-        wr_nvrd  =$13                  ;NVRAM data port
-
-;================================================================================
-;
-;MAXIM DS1511 WATCHDOG TIMER INITIALIZATION DATA
-;
-;       These tables consist of register initialization bit patterns & corresp-
-;       onding register offsets, & are read backwards,  requiring that register
-;       values be in the reverse order in which they should be set up.
-;
-;       Caution should be exercised in changing the order of these  tables,  as
-;       doing so may cause the device to malfunction.  Code at the end of these
-;       tables will emit an error message if there is a  mismatch  between  the
-;       number of registers & number of parameters.
-;
-;
-;       registers...
-;
-rtcreg   .byte wr_crb          ;control B
-         .byte wr_seca         ;alarm sec
-         .byte wr_mina         ;alarm min
-         .byte wr_hrsa         ;alarm hour
-         .byte wr_dowa         ;alarm date/day
-         .byte wr_wdms         ;watchdog msecs * 10
-         .byte wr_wds          ;watchdog secs
-         .byte wr_crb          ;control B
-n_rtcreg =*-rtcreg
-;
-;
-;       parameters...
-;
-rtcparm  .byte wr_crbpb        ;updates & WDT IRQs on
-         .byte wr_secap        ;no alarm secs IRQ
-         .byte wr_minap        ;no alarm min IRQ
-         .byte wr_hrsap        ;no alarm hour IRQ
-         .byte wr_dowap        ;no alarm date/day IRQ
-         .byte wr_wdmsp        ;10 ms underflows LSB
-         .byte wr_wdsp         ;10 ms underflows MSB
-         .byte wr_crbpa        ;updates & WDT IRQs off
-;
-        .if *-rtcparm < n_rtcreg
-                .error "!!! RTCREG & RTCPARM data tables don't match !!!"
-        .endif
-        .if *-rtcparm > n_rtcreg
-                .error "!!! RTCREG & RTCPARM data tables don't match !!!"
-        .endif
-;
+        .include "1511.asm"
+        .include "1511_constants.asm"
 
 DEBUGRTC   .macro
         PHA
@@ -274,7 +109,8 @@ alarm   .proc
 constime    .proc
         .pend
 
-; 
+;
+; Read date and time and store in compressed format
 ;================================================================================ 
 ; 
 ;getdtr: READ RTC DATE & TIME REGISTERS 
@@ -313,17 +149,45 @@ constime    .proc
 ;            jsr getdtr 
 ;    
 ; 
-getdtr  .proc
-        lda wr_mont
+getdtr  .block
+        PHA
+        PHX
+        LDA  CRB_RTC
+        PHA             ;Preserve control register B
+        AND  #D11SUMSK  ;Turn off update of registers
+        STA  CRB_RTC    
+        LDX  #0         ;Initialise index
+L1
+        LDA  IO_RTC,X   ;Read time data
+        CPX  #WR_MON    ;Month byte contains control bits
+        BNE  L2
+        AND  #d11emmsk  ;Get rid of control bits
+L2      STA  TODBUF,X
+        INX
+        CPX  #WR_SECA   ;IF X != Alarm seconds register
+        BNE  L1         ;  GOTO L1, next register
+        PLA             ;  ELSE, we're done 
+        STA  CRB_RTC    ;  restore all registers
+        PLX
+        PLA
+        RTS
         ; split into hi and lo byte
         ; or high with seconds
         ; or lo with minutes
-        lda wr_mint
         ; split into hi and lo byte
         ; or high with year
         ; or lo with day 
-        .pend
+        .bend
 
+; Convert TODBUF to compressed binary format 
+compress    .proc
+        LDA  TODBUF+WR_SEC
+        JSR  BCD2BIN
+        
+        LDA  TODBUF+WR_MON
+        AND  #%00010000     ; MSB month
+        
+        .pend
 ; 
 ;================================================================================ 
 ; 
